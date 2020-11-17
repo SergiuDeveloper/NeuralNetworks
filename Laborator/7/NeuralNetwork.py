33fb0bdb63b0dd4818c6cb1321bb5e254f506521
SergiuDev <sergiu.nistor@info.uaic.ro>
2020-11-18 01:50:14 +0200

    Added softmax for the last layer

--- Laborator/7/NeuralNetwork.py
+++ Laborator/7/NeuralNetwork.py
@@ -9,14 +9,14 @@ class NeuralNetwork:
         self.__w_output = np.array((0, 0))
 
     def fit(self, X_train, X_test, max_epochs, learning_rate):
-        self.__w_hidden = np.random.uniform(low=-0.5, high=0.5, size=(784, 100))
-        self.__w_output = np.random.uniform(low=-0.5, high=0.5, size=(100, 10))
+        self.__w_hidden = np.random.uniform(low=-1, high=1, size=(784, 100))
+        self.__w_output = np.random.uniform(low=-1, high=1, size=(100, 10))
 
         for epoch in range(1, max_epochs + 1):
             l1_in, l1_out, l2_in, l2_out = self.__forward_prop(X_train)
 
             l2_err = l2_out - X_test
-            l2_delta = np.dot(l1_out.T, l2_err * self.__sigmoid_derivative(l2_in))
+            l2_delta = np.dot(l1_out.T, l2_err * self.__softmax_derivative(l2_in))
 
             l1_err = np.dot(l2_err * self.__sigmoid_derivative(l2_in), self.__w_output.T)
             l1_delta = np.dot(X_train.T, self.__sigmoid_derivative(l1_in) * l1_err)
@@ -43,22 +43,25 @@ class NeuralNetwork:
 
     def __cross_entropy(self, X_train, X_test):
         forward_prop_result = self.__forward_prop(X_train)[3]
-        return -1 / X_train.shape[0] * np.sum(X_test * np.log(forward_prop_result) + (1 - X_test) * np.log(1 - forward_prop_result))
+        return - 1 / (X_train.shape[0] * np.sum(X_test * np.log(forward_prop_result) + (1 - X_test) * np.log(1 - forward_prop_result)))
 
     def __forward_prop(self, X_train):
         l1_in = np.dot(X_train, self.__w_hidden)
         l1_out = self.__sigmoid_activation(l1_in)
         l2_in = np.dot(l1_out, self.__w_output)
-        l2_out = self.__sigmoid_activation(l2_in)
+        l2_out = self.__softmax__activation(l2_in)
 
         return l1_in, l1_out, l2_in, l2_out
 
     def __sigmoid_activation(self, val):
-        try:
-            sig = 1 / (1 + np.exp(-val))
-            return sig
-        except FloatingPointError:
-            return 1
+        return 1 / (1 + np.exp(-val))
 
     def __sigmoid_derivative(self, val):
         return self.__sigmoid_activation(val) * (1 - self.__sigmoid_activation(val))
+
+    def __softmax__activation(self, val):
+        exps = np.exp(val - val.max())
+        return exps / np.sum(exps)
+
+    def __softmax_derivative(self, val):
+        return - np.sum(self.__softmax__activation(val)) / val.shape[0]
